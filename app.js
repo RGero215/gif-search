5c5ae5abdf29d9ffa5105561e3fd6abedc2d45c3
Ramon Geronimo <rgero215@gmail.com>
2018-10-22 18:20:54 -0700

    add error handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var exphbs  = require('express-handlebars');
 var http = require('http');
 // INITIALIZE THE GIPHY-API LIBRARY
 var giphy = require('giphy-api')();
+
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000
@@ -14,30 +15,39 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    console.log(req.query.term)
-    var queryString = req.query.term;
-    // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
-    var term = encodeURIComponent(queryString);
-    // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+    // console.log(req.query.term)
+    // var queryString = req.query.term;
+    // // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+    // var term = encodeURIComponent(queryString);
+    // // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+    // var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
 
-    http.get(url, function(response) {
-        // SET ENCODING OF RESPONSE TO UTF8
-        response.setEncoding('utf8');
+    // http.get(url, function(response) {
+    //     // SET ENCODING OF RESPONSE TO UTF8
+    //     response.setEncoding('utf8');
 
-        var body = '';
+    //     var body = '';
 
-        response.on('data', function(d) {
-        // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
-        body += d;
-        });
+    //     response.on('data', function(d) {
+    //     // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+    //     body += d;
+    //     });
 
-        response.on('end', function() {
-        // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
-        var parsed = JSON.parse(body);
-        // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
-        res.render('home', {gifs: parsed.data})
-        });
+    //     response.on('end', function() {
+    //     // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+    //     var parsed = JSON.parse(body);
+    //     // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+    //     res.render('home', {gifs: parsed.data})
+    //     });
+    // });
+
+    giphy.search(req.query.term, function (err, response) {
+        if(!err){
+            res.render('home', {gifs: response.data})
+        } else {
+            res.render('home')
+        }
+        
     });
 })
     
